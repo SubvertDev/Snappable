47af17dfa4a5f57771ef41ae629ce90818cbc7e8
Maxim Mukhortov <m.myxortov2011@gmail.com>
2025-01-15 14:33:17 +0300

    Update swiftui-introspect version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/siteline/swiftui-introspect.git", from: "1.1.3")
+        .package(url: "https://github.com/siteline/swiftui-introspect.git", from: "1.3.0")
     ],
     targets: [
         .target(
